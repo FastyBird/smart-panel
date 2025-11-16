a72f3e2dae3f222ae647deaf107187453cb2d183
Adam Kadlec <adam.kadlec@gmail.com>
2025-11-17 00:28:22 +0100

    Allow to change channel and property category

--- apps/backend/src/plugins/devices-shelly-v1/dto/update-channel-property.dto.ts
+++ apps/backend/src/plugins/devices-shelly-v1/dto/update-channel-property.dto.ts
@@ -1,6 +1,7 @@
 import { Expose } from 'class-transformer';
-import { IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
+import { PropertyCategory } from '../../../modules/devices/devices.constants';
 import { UpdateChannelPropertyDto } from '../../../modules/devices/dto/update-channel-property.dto';
 import { DEVICES_SHELLY_V1_TYPE } from '../devices-shelly-v1.constants';
 
@@ -8,4 +9,14 @@ export class UpdateShellyV1ChannelPropertyDto extends UpdateChannelPropertyDto {
 	@Expose()
 	@IsString({ message: '[{"field":"type","reason":"Type must be a valid channel property type string."}]' })
 	readonly type: typeof DEVICES_SHELLY_V1_TYPE;
+
+	@Expose()
+	@IsOptional()
+	@IsNotEmpty({
+		message: '[{"field":"category","reason":"Category must be a valid property category."}]',
+	})
+	@IsEnum(PropertyCategory, {
+		message: '[{"field":"category","reason":"Category must be a valid property category."}]',
+	})
+	category?: PropertyCategory;
 }
--- apps/backend/src/plugins/devices-shelly-v1/dto/update-channel.dto.ts
+++ apps/backend/src/plugins/devices-shelly-v1/dto/update-channel.dto.ts
@@ -1,6 +1,7 @@
 import { Expose } from 'class-transformer';
-import { IsString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
+import { ChannelCategory } from '../../../modules/devices/devices.constants';
 import { UpdateChannelDto } from '../../../modules/devices/dto/update-channel.dto';
 import { DEVICES_SHELLY_V1_TYPE } from '../devices-shelly-v1.constants';
 
@@ -8,4 +9,14 @@ export class UpdateShellyV1ChannelDto extends UpdateChannelDto {
 	@Expose()
 	@IsString({ message: '[{"field":"type","reason":"Type must be a valid channel type string."}]' })
 	readonly type: typeof DEVICES_SHELLY_V1_TYPE;
+
+	@Expose()
+	@IsOptional()
+	@IsNotEmpty({
+		message: '[{"field":"category","reason":"Category must be a valid channel category."}]',
+	})
+	@IsEnum(ChannelCategory, {
+		message: '[{"field":"category","reason":"Category must be a valid channel category."}]',
+	})
+	category?: ChannelCategory;
 }
