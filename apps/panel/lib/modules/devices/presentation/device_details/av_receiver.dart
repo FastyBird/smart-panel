fc481b989a025a49b325fe89d8412f736d1c2dd9
Adam Kadlec <adam.kadlec@gmail.com>
2026-02-02 09:20:35 +0100

    refactor(panel): Extract duplicated _inputSourceLabel to shared utility and remove unused media detail page

--- apps/panel/lib/modules/devices/presentation/device_details/av_receiver.dart
+++ apps/panel/lib/modules/devices/presentation/device_details/av_receiver.dart
@@ -10,6 +10,7 @@ import 'package:fastybird_smart_panel/core/widgets/device_detail_portrait_layout
 import 'package:fastybird_smart_panel/core/widgets/device_offline_overlay.dart';
 import 'package:fastybird_smart_panel/core/widgets/page_header.dart';
 import 'package:fastybird_smart_panel/l10n/app_localizations.dart';
+import 'package:fastybird_smart_panel/modules/devices/presentation/utils/media_input_source_label.dart';
 import 'package:fastybird_smart_panel/modules/devices/presentation/widgets/media_info_card.dart';
 import 'package:fastybird_smart_panel/modules/devices/presentation/widgets/media_playback_card.dart';
 import 'package:fastybird_smart_panel/modules/devices/presentation/widgets/media_source_card.dart';
@@ -349,7 +350,7 @@ class _AvReceiverDeviceDetailState extends State<AvReceiverDeviceDetail> {
 		final source = _device.mediaInputSource;
 		if (source != null) {
 			if (_device.mediaInputAvailableSources.isNotEmpty) {
-				return _inputSourceLabel(context, source);
+				return mediaInputSourceLabel(context, source);
 			}
 			return source;
 		}
@@ -372,7 +373,7 @@ class _AvReceiverDeviceDetailState extends State<AvReceiverDeviceDetail> {
 		if (source == null) return null;
 
 		if (_device.mediaInputAvailableSources.isNotEmpty) {
-			return _inputSourceLabel(context, source);
+			return mediaInputSourceLabel(context, source);
 		}
 
 		return source;
@@ -390,48 +391,6 @@ class _AvReceiverDeviceDetailState extends State<AvReceiverDeviceDetail> {
 			return isDark ? AppColorsDark.infoLight5 : AppColorsLight.infoLight5;
 		}
 		return isDark ? AppFillColorDark.darker : AppFillColorLight.darker;
-	}
-
-	String _inputSourceLabel(BuildContext context, String source) {
-		final localizations = AppLocalizations.of(context)!;
-		final key = 'media_input_$source';
-		switch (key) {
-			case 'media_input_hdmi1': return localizations.media_input_hdmi1;
-			case 'media_input_hdmi2': return localizations.media_input_hdmi2;
-			case 'media_input_hdmi3': return localizations.media_input_hdmi3;
-			case 'media_input_hdmi4': return localizations.media_input_hdmi4;
-			case 'media_input_hdmi5': return localizations.media_input_hdmi5;
-			case 'media_input_hdmi6': return localizations.media_input_hdmi6;
-			case 'media_input_arc': return localizations.media_input_arc;
-			case 'media_input_earc': return localizations.media_input_earc;
-			case 'media_input_tv': return localizations.media_input_tv;
-			case 'media_input_cable': return localizations.media_input_cable;
-			case 'media_input_satellite': return localizations.media_input_satellite;
-			case 'media_input_antenna': return localizations.media_input_antenna;
-			case 'media_input_av1': return localizations.media_input_av1;
-			case 'media_input_av2': return localizations.media_input_av2;
-			case 'media_input_component': return localizations.media_input_component;
-			case 'media_input_vga': return localizations.media_input_vga;
-			case 'media_input_dvi': return localizations.media_input_dvi;
-			case 'media_input_usb': return localizations.media_input_usb;
-			case 'media_input_bluetooth': return localizations.media_input_bluetooth;
-			case 'media_input_wifi': return localizations.media_input_wifi;
-			case 'media_input_airplay': return localizations.media_input_airplay;
-			case 'media_input_cast': return localizations.media_input_cast;
-			case 'media_input_dlna': return localizations.media_input_dlna;
-			case 'media_input_miracast': return localizations.media_input_miracast;
-			case 'media_input_app_netflix': return localizations.media_input_app_netflix;
-			case 'media_input_app_youtube': return localizations.media_input_app_youtube;
-			case 'media_input_app_spotify': return localizations.media_input_app_spotify;
-			case 'media_input_app_prime_video': return localizations.media_input_app_prime_video;
-			case 'media_input_app_disney_plus': return localizations.media_input_app_disney_plus;
-			case 'media_input_app_hbo_max': return localizations.media_input_app_hbo_max;
-			case 'media_input_app_apple_tv': return localizations.media_input_app_apple_tv;
-			case 'media_input_app_plex': return localizations.media_input_app_plex;
-			case 'media_input_app_kodi': return localizations.media_input_app_kodi;
-			case 'media_input_other': return localizations.media_input_other;
-			default: return source;
-		}
 	}
 
 	// --------------------------------------------------------------------------
@@ -585,7 +544,7 @@ class _AvReceiverDeviceDetailState extends State<AvReceiverDeviceDetail> {
 							currentSource: _device.mediaInputSource,
 							availableSources: _device.mediaInputAvailableSources,
 							isEnabled: _isOn,
-							sourceLabel: (s) => _inputSourceLabel(context, s),
+							sourceLabel: (s) => mediaInputSourceLabel(context, s),
 							onSourceChanged: _setSource,
 							scale: _scale,
 						),
@@ -641,7 +600,7 @@ class _AvReceiverDeviceDetailState extends State<AvReceiverDeviceDetail> {
 							currentSource: _device.mediaInputSource,
 							availableSources: _device.mediaInputAvailableSources,
 							isEnabled: _isOn,
-							sourceLabel: (s) => _inputSourceLabel(context, s),
+							sourceLabel: (s) => mediaInputSourceLabel(context, s),
 							onSourceChanged: _setSource,
 							scale: _scale,
 						),
--- apps/panel/lib/modules/devices/presentation/device_details/television.dart
+++ apps/panel/lib/modules/devices/presentation/device_details/television.dart
@@ -10,6 +10,7 @@ import 'package:fastybird_smart_panel/core/widgets/device_detail_portrait_layout
 import 'package:fastybird_smart_panel/core/widgets/device_offline_overlay.dart';
 import 'package:fastybird_smart_panel/core/widgets/page_header.dart';
 import 'package:fastybird_smart_panel/l10n/app_localizations.dart';
+import 'package:fastybird_smart_panel/modules/devices/presentation/utils/media_input_source_label.dart';
 import 'package:fastybird_smart_panel/modules/devices/presentation/widgets/media_brightness_card.dart';
 import 'package:fastybird_smart_panel/modules/devices/presentation/widgets/media_info_card.dart';
 import 'package:fastybird_smart_panel/modules/devices/presentation/widgets/media_remote_card.dart';
@@ -373,7 +374,7 @@ class _TelevisionDeviceDetailState extends State<TelevisionDeviceDetail> {
 		final source = _device.mediaInputSource;
 		if (source != null) {
 			if (_device.mediaInputAvailableSources.isNotEmpty) {
-				return _inputSourceLabel(context, source);
+				return mediaInputSourceLabel(context, source);
 			}
 			return source;
 		}
@@ -403,52 +404,10 @@ class _TelevisionDeviceDetailState extends State<TelevisionDeviceDetail> {
 		if (source == null) return null;
 
 		if (_device.mediaInputAvailableSources.isNotEmpty) {
-			return _inputSourceLabel(context, source);
+			return mediaInputSourceLabel(context, source);
 		}
 
 		return source;
-	}
-
-	String _inputSourceLabel(BuildContext context, String source) {
-		final localizations = AppLocalizations.of(context)!;
-		final key = 'media_input_$source';
-		switch (key) {
-			case 'media_input_hdmi1': return localizations.media_input_hdmi1;
-			case 'media_input_hdmi2': return localizations.media_input_hdmi2;
-			case 'media_input_hdmi3': return localizations.media_input_hdmi3;
-			case 'media_input_hdmi4': return localizations.media_input_hdmi4;
-			case 'media_input_hdmi5': return localizations.media_input_hdmi5;
-			case 'media_input_hdmi6': return localizations.media_input_hdmi6;
-			case 'media_input_arc': return localizations.media_input_arc;
-			case 'media_input_earc': return localizations.media_input_earc;
-			case 'media_input_tv': return localizations.media_input_tv;
-			case 'media_input_cable': return localizations.media_input_cable;
-			case 'media_input_satellite': return localizations.media_input_satellite;
-			case 'media_input_antenna': return localizations.media_input_antenna;
-			case 'media_input_av1': return localizations.media_input_av1;
-			case 'media_input_av2': return localizations.media_input_av2;
-			case 'media_input_component': return localizations.media_input_component;
-			case 'media_input_vga': return localizations.media_input_vga;
-			case 'media_input_dvi': return localizations.media_input_dvi;
-			case 'media_input_usb': return localizations.media_input_usb;
-			case 'media_input_bluetooth': return localizations.media_input_bluetooth;
-			case 'media_input_wifi': return localizations.media_input_wifi;
-			case 'media_input_airplay': return localizations.media_input_airplay;
-			case 'media_input_cast': return localizations.media_input_cast;
-			case 'media_input_dlna': return localizations.media_input_dlna;
-			case 'media_input_miracast': return localizations.media_input_miracast;
-			case 'media_input_app_netflix': return localizations.media_input_app_netflix;
-			case 'media_input_app_youtube': return localizations.media_input_app_youtube;
-			case 'media_input_app_spotify': return localizations.media_input_app_spotify;
-			case 'media_input_app_prime_video': return localizations.media_input_app_prime_video;
-			case 'media_input_app_disney_plus': return localizations.media_input_app_disney_plus;
-			case 'media_input_app_hbo_max': return localizations.media_input_app_hbo_max;
-			case 'media_input_app_apple_tv': return localizations.media_input_app_apple_tv;
-			case 'media_input_app_plex': return localizations.media_input_app_plex;
-			case 'media_input_app_kodi': return localizations.media_input_app_kodi;
-			case 'media_input_other': return localizations.media_input_other;
-			default: return source;
-		}
 	}
 
 	// --------------------------------------------------------------------------
@@ -598,7 +557,7 @@ class _TelevisionDeviceDetailState extends State<TelevisionDeviceDetail> {
 							currentSource: _device.mediaInputSource,
 							availableSources: _device.mediaInputAvailableSources,
 							isEnabled: _device.isTelevisionOn,
-							sourceLabel: (s) => _inputSourceLabel(context, s),
+							sourceLabel: (s) => mediaInputSourceLabel(context, s),
 							onSourceChanged: _setSource,
 							scale: _scale,
 						),
@@ -672,7 +631,7 @@ class _TelevisionDeviceDetailState extends State<TelevisionDeviceDetail> {
 							currentSource: _device.mediaInputSource,
 							availableSources: _device.mediaInputAvailableSources,
 							isEnabled: _device.isTelevisionOn,
-							sourceLabel: (s) => _inputSourceLabel(context, s),
+							sourceLabel: (s) => mediaInputSourceLabel(context, s),
 							onSourceChanged: _setSource,
 							scale: _scale,
 						),
