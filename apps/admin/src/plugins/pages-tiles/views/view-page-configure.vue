62a952cf0fe4eebdc26483ae71444af596298cd9
Adam Kadlec <adam.kadlec@gmail.com>
2026-02-28 14:11:45 +0100

    fix(pages-tiles): Revert data source UI changes from pages-tiles plugin

--- apps/admin/src/plugins/pages-tiles/views/view-page-configure.vue
+++ apps/admin/src/plugins/pages-tiles/views/view-page-configure.vue
@@ -83,7 +83,6 @@
 		@select-display="onDisplaySelect"
 		@add-tile-of-type="onTileAdd"
 		@add-page-data-source="onPageDataSourceAdd"
-		@edit-page-data-source="onPageDataSourceEdit"
 		@edit-tile="onTileEdit"
 		@tile-detail="onTileDetail"
 	/>
@@ -150,7 +149,7 @@ import { ElButton, ElDrawer, ElDropdown, ElDropdownItem, ElDropdownMenu, ElIcon,
 import { Icon } from '@iconify/vue';
 
 import { AppBar, AppBarButton, AppBarButtonAlign, ViewError, useBreakpoints } from '../../../common';
-import { type IDataSource, RouteNames as DashboardRouteNames, type ITile } from '../../../modules/dashboard';
+import { RouteNames as DashboardRouteNames, type ITile } from '../../../modules/dashboard';
 import { type IDisplay, useDisplays } from '../../../modules/displays';
 import PageConfigure from '../components/page-configure.vue';
 import { RouteNames } from '../pages-tiles.constants';
@@ -315,22 +314,6 @@ const onPageDataSourceAdd = (): void => {
 				id: props.page.id,
 			},
 		});
-	}
-};
-
-const onPageDataSourceEdit = (dataSourceId: IDataSource['id']): void => {
-	const location = {
-		name: RouteNames.PAGE_EDIT_DATA_SOURCE,
-		params: {
-			id: props.page.id,
-			dataSourceId,
-		},
-	};
-
-	if (isLGDevice.value) {
-		router.replace(location);
-	} else {
-		router.push(location);
 	}
 };
 
