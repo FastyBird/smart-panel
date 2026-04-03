b59fa18a6811f29f6fbec8449bb6ea4210a3106c
Adam Kadlec <adam.kadlec@gmail.com>
2026-04-03 09:32:26 +0200

    fix(admin): only show enabled plugins in storage selector

--- apps/admin/src/modules/storage/composables/useStoragePlugins.ts
+++ apps/admin/src/modules/storage/composables/useStoragePlugins.ts
@@ -3,14 +3,16 @@ import { computed } from 'vue';
 import { orderBy } from 'natural-orderby';
 
 import { type IPlugin, injectPluginsManager } from '../../../common';
+import { useConfigPlugins } from '../../config/composables/useConfigPlugins';
 import { STORAGE_MODULE_NAME } from '../storage.constants';
 
 export const useStoragePlugins = () => {
 	const pluginsManager = injectPluginsManager();
+	const { enabled } = useConfigPlugins();
 
 	const plugins = computed<IPlugin[]>((): IPlugin[] => {
 		return orderBy(
-			pluginsManager.getPlugins().filter((plugin) => plugin.modules?.includes(STORAGE_MODULE_NAME)),
+			pluginsManager.getPlugins().filter((plugin) => plugin.modules?.includes(STORAGE_MODULE_NAME) && enabled(plugin.type)),
 			[(plugin) => plugin.name],
 			['asc'],
 		);
